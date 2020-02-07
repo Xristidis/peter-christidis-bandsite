32d242ecad3c5d5bf8e57349802ae87144961e9f
Peter Christidis <peter.xristidis@gmail.com>
2020-02-07 01:40:38 -0500

    minor changes to api call for post and get

--- band-sprint-three/javascript/index-script.js
+++ band-sprint-three/javascript/index-script.js
@@ -1,28 +1,28 @@
 const bandSite_API = "https://project-1-api.herokuapp.com";
 
 
+/// sort api array based on timestamp. 
 
 
-
-let commContent = [
-    {
-        name: "Michael Lyons",
-        date: Date.parse('12/18/2018'),
-        // PARSED THE DATE STRING INTO A FUNCTION THAT TURNED IT INTO MILSECONDS 
-        content: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
-    },
-    {
-        name: "Gary Wong",
-        date: Date.parse('12/12/2018'),
-        content: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
-    },
-    {
-        name: "Theodore Duncan",
-        date: Date.parse('11/15/2018'),
+// let commContent = [
+//     {
+//         name: "Michael Lyons",
+//         date: Date.parse('12/18/2018'),
+//         // PARSED THE DATE STRING INTO A FUNCTION THAT TURNED IT INTO MILSECONDS 
+//         content: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+//     },
+//     {
+//         name: "Gary Wong",
+//         date: Date.parse('12/12/2018'),
+//         content: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+//     },
+//     {
+//         name: "Theodore Duncan",
+//         date: Date.parse('11/15/2018'),
         
-        content: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
-    }
-];
+//         content: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
+//     }
+// ];
 
 function timeSince(date) {
     var seconds = Math.floor((new Date() - date) / 1000);
@@ -51,10 +51,22 @@ function timeSince(date) {
   var aDay = 24*60*60*1000;
   console.log(timeSince(new Date(Date.now()-aDay)));
   console.log(timeSince(new Date(Date.now()-aDay*2)));
-    const loadComments = () => {
+  ///////////// AXIOS GET REQUEST WORKS ///////////////
+  axios.get('https://project-1-api.herokuapp.com/comments?api_key=32c5cf26-1bc6-4b6a-9a79-cf829fa9c21a', {
+        
+  }).then(res => {
+      console.log(res.data);
+      loadComments(res.data);
+  }).catch(err => {
+      console.log(err);
+  });
+  ///////////////////////////////////////////////////////////
+
+    const loadComments = (arr) => {
     const commPastParent = document.querySelector(".comments__past-wrapper");
 
-  commContent.forEach(commContentObj => {
+  arr.forEach(commContentObj => {
+
         // COMMENTS PAST SHELL
       let commPast = document.createElement('div');
       commPast.classList.add("comments__past");
@@ -76,15 +88,15 @@ function timeSince(date) {
           let commDate = document.createElement('div');
           commDate.classList.add("comments__date");
           commInfo.appendChild(commDate);
-          commDate.innerText = timeSince(commContentObj.date);
+          commDate.innerText = timeSince(commContentObj.timestamp);
           // COMMENTS CONTENT
           let commContent = document.createElement('div');
           commContent.classList.add("comments__past-comment");
           commInfo.appendChild(commContent);
-          commContent.innerText = commContentObj.content;
+          commContent.innerText = commContentObj.comment;
       });
   };
-  loadComments();
+  // loadComments();
   
   const form = document.querySelector('form');
   // SETS 'FORM' AS THE ELEMENT FOR THE LISTENER
@@ -98,14 +110,17 @@ function timeSince(date) {
       commContent.unshift(newCommObject);
       console.log(commContent);
 
+      ///////AXIOS POST REQUESTS///////////////
       axios.post('https://project-1-api.herokuapp.com/comments?api_key=32c5cf26-1bc6-4b6a-9a79-cf829fa9c21a', {
         name: newCommObject.name,
-        comment: newCommObject.content
+        comment: newCommObject.comment
     }).then(res => {
         console.log(res.data);
+        loadComments();/////////////////
     }).catch(err => {
         console.log(err);
     });
+    /////////////////////////////////////////////
       clearAll();
       loadComments();
       form.reset();
@@ -125,19 +140,3 @@ function timeSince(date) {
 
       let apiForm = document.querySelector('form');
 console.log(form);
-
-// apiForm.addEventListener('submit', (evt) => {
-//     evt.preventDefault();
-
-//     let name = evt.target.name.value;
-//     let comment = evt.target.comment.value;
-
-//     axios.post('https://project-1-api.herokuapp.com/comments?api_key=32c5cf26-1bc6-4b6a-9a79-cf829fa9c21a', {
-//         name: name,
-//         comment: comment
-//     }).then(res => {
-//         console.log(res.data);
-//     }).catch(err => {
-//         console.log(err);
-//     });
-// });
