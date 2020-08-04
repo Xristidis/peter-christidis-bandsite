866b6403b95acd5f0cfefe7547e6fe159168c492
Peter Christidis <peter.xristidis@gmail.com>
2020-08-03 23:14:33 -0400

    changed copy of site

--- band-sprint-three/javascript/index-script.js
+++ band-sprint-three/javascript/index-script.js
@@ -1,163 +1,142 @@
 const bandSite_API = "peters_api_key"
 
 function getFunc(param) {
-let sortArr = []
-      param.forEach(i => {
-        sortArr.push(i)
-      })
-      let revArr = sortArr.reverse();
-      console.log(revArr); 
-      loadComments(revArr);
-    }
-/// sort api array based on timestamp. 
-
-
-// let commContent = [
-//     {
-//         name: "Michael Lyons",
-//         date: Date.parse('12/18/2018'),
-//         // PARSED THE DATE STRING INTO A FUNCTION THAT TURNED IT INTO MILSECONDS 
-//         content: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
-//     },
-//     {
-//         name: "Gary Wong",
-//         date: Date.parse('12/12/2018'),
-//         content: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
-//     },
-//     {
-//         name: "Theodore Duncan",
-//         date: Date.parse('11/15/2018'),
-        
-//         content: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!"
-//     }
-// ];
+  let sortArr = []
+  param.forEach(i => {
+    sortArr.push(i)
+  })
+  let revArr = sortArr.reverse();
+  console.log(revArr);
+  loadComments(revArr);
+}
+// sort api array based on timestamp. 
 
 function timeSince(date) {
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var interval = Math.floor(seconds / 31536000);
-    if (interval > 1) {
-      return interval + " years ago";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-      return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-      return interval + " days ago";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-      return interval + " hours ago";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-      return interval + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = Math.floor(seconds / 31536000);
+  if (interval > 1) {
+    return interval + " years ago";
   }
-  var aDay = 24*60*60*1000;
-  console.log(timeSince(new Date(Date.now()-aDay)));
-  console.log(timeSince(new Date(Date.now()-aDay*2)));
-  ///////////// AXIOS GET REQUEST WORKS ///////////////
-  axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${bandSite_API}`, {
-        
-  }).then(res => {
-    getFunc(res.data);
-  }).catch(err => {
-      console.log(err);
-  });
-  ///////////////////////////////////////////////////////////
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+}
+var aDay = 24 * 60 * 60 * 1000;
+console.log(timeSince(new Date(Date.now() - aDay)));
+console.log(timeSince(new Date(Date.now() - aDay * 2)));
+///////////// AXIOS GET REQUEST WORKS ///////////////
+axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${bandSite_API}`, {
 
-    const loadComments = (arr) => {
-    const commPastParent = document.querySelector(".comments__past-wrapper");
+}).then(res => {
+  getFunc(res.data);
+}).catch(err => {
+  console.log(err);
+});
+///////////////////////////////////////////////////////////
+
+const loadComments = (arr) => {
+  const commPastParent = document.querySelector(".comments__past-wrapper");
 
   arr.forEach(commContentObj => {
 
-        // COMMENTS PAST SHELL
-      let commPast = document.createElement('div');
-      commPast.classList.add("comments__past");
-      commPastParent.appendChild(commPast);
-      // COMMENTS PAST PHOTO
-      let commPastPhoto = document.createElement('div');
-      commPastPhoto.classList.add("comments__past-photo");
-      commPast.appendChild(commPastPhoto);
-      // COMMENTS INFO 
-      let commInfo = document.createElement('div');
-      commInfo.classList.add("comments__info");
-      commPast.appendChild(commInfo);
-          // COMMENTS NAME
-          let commName = document.createElement('div');
-          commName.classList.add("comments__name");
-          commInfo.appendChild(commName);
-          commName.innerText = commContentObj.name;
-          // COMMENTS DATE
-          let commDate = document.createElement('div');
-          commDate.classList.add("comments__date");
-          commInfo.appendChild(commDate);
-          commDate.innerText = timeSince(commContentObj.timestamp);
-          // COMMENTS CONTENT
-          let commContent = document.createElement('div');
-          commContent.classList.add("comments__past-comment");
-          commInfo.appendChild(commContent);
-          commContent.innerText = commContentObj.comment;
-      });
-  };
-  
-  const form = document.querySelector('form');
-  // SETS 'FORM' AS THE ELEMENT FOR THE LISTENER
-  form.addEventListener('submit', (eventObj) => {
-      eventObj.preventDefault();
-      let newCommObject = {};
-      newCommObject.name = eventObj.target.name.value;
-      // CREATES A TIME STAMP VALUE FOR WHEN THE SUBMIT BUTTON IS PRESSED
-      newCommObject.date = Date.now();
-      newCommObject.comment = eventObj.target.comment.value;
-      // commContent.unshift(newCommObject); ////////// NEED SYNTAX FOR API ARRAY
-      // console.log(commContent);
-
-      ///////AXIOS POST REQUESTS///////////////
-      axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${bandSite_API}`, {
-        name: newCommObject.name,
-        comment: newCommObject.comment
-    }).then(res => {
-        axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${bandSite_API}`, {
-          
-        }).then(res => {
-            // let sortArr = []
-            // res.data.forEach(i => {
-            //   sortArr.push(i)
-            // })
-            // let revArr = sortArr.reverse(); ///// CHANGE THIS TO SORT BY TIMESTAMP ****
-            // console.log(revArr); 
-            // res.data.sort 
-            // console.log(res.data);
-            getFunc(res.data);
-            // loadComments(revArr);
-        }).catch(err => {
-            console.log(err);
-        });
-        console.log(res);
-        // loadComments(res);/////////////////
-    }).catch(err => {
-        console.log(err);
-    });
-    /////////////////////////////////////////////
-      clearAll();
-      form.reset();
+    // COMMENTS PAST SHELL
+    let commPast = document.createElement('div');
+    commPast.classList.add("comments__past");
+    commPastParent.appendChild(commPast);
+    // COMMENTS PAST PHOTO
+    let commPastPhoto = document.createElement('div');
+    commPastPhoto.classList.add("comments__past-photo");
+    commPast.appendChild(commPastPhoto);
+    // COMMENTS INFO 
+    let commInfo = document.createElement('div');
+    commInfo.classList.add("comments__info");
+    commPast.appendChild(commInfo);
+    // COMMENTS NAME
+    let commName = document.createElement('div');
+    commName.classList.add("comments__name");
+    commInfo.appendChild(commName);
+    commName.innerText = commContentObj.name;
+    // COMMENTS DATE
+    let commDate = document.createElement('div');
+    commDate.classList.add("comments__date");
+    commInfo.appendChild(commDate);
+    commDate.innerText = timeSince(commContentObj.timestamp);
+    // COMMENTS CONTENT
+    let commContent = document.createElement('div');
+    commContent.classList.add("comments__past-comment");
+    commInfo.appendChild(commContent);
+    commContent.innerText = commContentObj.comment;
   });
+};
 
-  function clearAll() {
-      //CREATED A PARENT
-      const newparent = document.querySelector(".comments__past-wrapper");
-      // CREATED A CHILD
-      let child = newparent.getElementsByClassName("comments__past");
-      // RAN A LOOP THE LENGTH OF ALL THE CURRENT COMMENTS. ON EACH ITERATION OF THE LOOP IT WOULD REMOVE THE CHILD ELEMENT (COMMENT).
-      for (let i = child.length; i > 0; i--) {
-          newparent.removeChild(child[i - 1]);
-          }
-      };
-      // RAN OUT OF TIME AND COULD NOT SEEM TO TURN MY FOR LOOP HERE INTO A FOREACH FUNCTION TO DELETE EACH COMMENT IN MY COMMENT SECTION.
+const form = document.querySelector('form');
+// SETS 'FORM' AS THE ELEMENT FOR THE LISTENER
+form.addEventListener('submit', (eventObj) => {
+  eventObj.preventDefault();
+  let newCommObject = {};
+  newCommObject.name = eventObj.target.name.value;
+  // CREATES A TIME STAMP VALUE FOR WHEN THE SUBMIT BUTTON IS PRESSED
+  newCommObject.date = Date.now();
+  newCommObject.comment = eventObj.target.comment.value;
+  // commContent.unshift(newCommObject); ////////// NEED SYNTAX FOR API ARRAY
+  // console.log(commContent);
 
-      let apiForm = document.querySelector('form');
+  ///////AXIOS POST REQUESTS///////////////
+  axios.post(`https://project-1-api.herokuapp.com/comments?api_key=${bandSite_API}`, {
+    name: newCommObject.name,
+    comment: newCommObject.comment
+  }).then(res => {
+    axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${bandSite_API}`, {
+
+    }).then(res => {
+      // let sortArr = []
+      // res.data.forEach(i => {
+      //   sortArr.push(i)
+      // })
+      // let revArr = sortArr.reverse(); ///// CHANGE THIS TO SORT BY TIMESTAMP ****
+      // console.log(revArr); 
+      // res.data.sort 
+      // console.log(res.data);
+      getFunc(res.data);
+      // loadComments(revArr);
+    }).catch(err => {
+      console.log(err);
+    });
+    console.log(res);
+    // loadComments(res);/////////////////
+  }).catch(err => {
+    console.log(err);
+  });
+  /////////////////////////////////////////////
+  clearAll();
+  form.reset();
+});
+
+function clearAll() {
+  //CREATED A PARENT
+  const newparent = document.querySelector(".comments__past-wrapper");
+  // CREATED A CHILD
+  let child = newparent.getElementsByClassName("comments__past");
+  // RAN A LOOP THE LENGTH OF ALL THE CURRENT COMMENTS. ON EACH ITERATION OF THE LOOP IT WOULD REMOVE THE CHILD ELEMENT (COMMENT).
+  for (let i = child.length; i > 0; i--) {
+    newparent.removeChild(child[i - 1]);
+  }
+};
+// TURN FOR LOOP HERE INTO A FOREACH FUNCTION TO DELETE EACH COMMENT IN MY COMMENT SECTION.
+
+let apiForm = document.querySelector('form');
 console.log(form);
--- band-sprint-three/javascript/show-script.js
+++ band-sprint-three/javascript/show-script.js
@@ -2,18 +2,18 @@ const bandSite_API = "peters_api_key"
 
 function getFunc(param) {
   let sortArr = []
-    param.forEach(i => {
-      sortArr.push(i)
-    })
-    console.log(sortArr); 
-    loadShows(sortArr);
-  }
+  param.forEach(i => {
+    sortArr.push(i)
+  })
+  console.log(sortArr);
+  loadShows(sortArr);
+}
 ///////////// AXIOS GET REQUEST ///////////////
 axios.get(`https://project-1-api.herokuapp.com/showdates?api_key=${bandSite_API}`, {
 }).then(res => {
   getFunc(res.data);
 }).catch(err => {
-    console.log(err);
+  console.log(err);
 });
 ////////////////////////////////////////////////
 const loadShows = (sortArr) => {
@@ -32,65 +32,64 @@ const loadShows = (sortArr) => {
   // BELOW, THE FOREACH LOOP WILL KEEP CREATING NEW ELEMENTS UNTIL THE ARRAY OF OBJECTS ENDS. 
   sortArr.forEach(showContentObj => {
     // TOUR WRAPPER
-  let tourWrapper = document.createElement("div");
-  tourWrapper.classList.add("tour__wrapper");
-  tourWrapper.classList.add("main__tour");
-  tourTable.appendChild(tourWrapper);
+    let tourWrapper = document.createElement("div");
+    tourWrapper.classList.add("tour__wrapper");
+    tourWrapper.classList.add("main__tour");
+    tourTable.appendChild(tourWrapper);
 
-  let tourSectionDate = document.createElement("div");
-  tourSectionDate.classList.add("tour__section");
-  tourSectionDate.classList.add("tour__date");
-  tourWrapper.appendChild(tourSectionDate);
-  // TOUR DATE
-  let tourDate = document.createElement("div");
-  tourDate.classList.add("date__input");
-  tourDate.classList.add("main__date");
-  tourSectionDate.appendChild(tourDate);
-  tourDate.innerText = "DATE";
+    let tourSectionDate = document.createElement("div");
+    tourSectionDate.classList.add("tour__section");
+    tourSectionDate.classList.add("tour__date");
+    tourWrapper.appendChild(tourSectionDate);
+    // TOUR DATE
+    let tourDate = document.createElement("div");
+    tourDate.classList.add("date__input");
+    tourDate.classList.add("main__date");
+    tourSectionDate.appendChild(tourDate);
+    tourDate.innerText = "DATE";
 
-  // TOUR DATE TITLE
-  let tourDateInfo = document.createElement("div");
-  tourDateInfo.classList.add("date__title");
-  tourSectionDate.appendChild(tourDateInfo);
-  tourDateInfo.innerText = showContentObj.date;
+    // TOUR DATE TITLE
+    let tourDateInfo = document.createElement("div");
+    tourDateInfo.classList.add("date__title");
+    tourSectionDate.appendChild(tourDateInfo);
+    tourDateInfo.innerText = showContentObj.date;
 
-  let tourSectionVenue = document.createElement("div");
-  tourSectionVenue.classList.add("tour__section");
-  tourSectionVenue.classList.add("tour__venue");
-  tourWrapper.appendChild(tourSectionVenue);
+    let tourSectionVenue = document.createElement("div");
+    tourSectionVenue.classList.add("tour__section");
+    tourSectionVenue.classList.add("tour__venue");
+    tourWrapper.appendChild(tourSectionVenue);
 
-  let tourVenue = document.createElement("div");
-  tourVenue.classList.add("venue__input");
-  tourVenue.classList.add("main__venue");
-  tourSectionVenue.appendChild(tourVenue);
-  tourVenue.innerText = "VENUE";
+    let tourVenue = document.createElement("div");
+    tourVenue.classList.add("venue__input");
+    tourVenue.classList.add("main__venue");
+    tourSectionVenue.appendChild(tourVenue);
+    tourVenue.innerText = "VENUE";
 
-  // TOUR VENUE TITLE
-  let tourVenueInfo = document.createElement("div");
-  tourVenueInfo.classList.add("venue__title");
-  tourSectionVenue.appendChild(tourVenueInfo);
-  tourVenueInfo.innerText = showContentObj.place;
+    // TOUR VENUE TITLE
+    let tourVenueInfo = document.createElement("div");
+    tourVenueInfo.classList.add("venue__title");
+    tourSectionVenue.appendChild(tourVenueInfo);
+    tourVenueInfo.innerText = showContentObj.place;
 
-  let tourSectionLocation = document.createElement("div");
-  tourSectionLocation.classList.add("tour__section");
-  tourSectionLocation.classList.add("tour__location");
-  tourWrapper.appendChild(tourSectionLocation);
+    let tourSectionLocation = document.createElement("div");
+    tourSectionLocation.classList.add("tour__section");
+    tourSectionLocation.classList.add("tour__location");
+    tourWrapper.appendChild(tourSectionLocation);
 
-  let tourLocation = document.createElement("div");
-  tourLocation.classList.add("location__input");
-  tourLocation.classList.add("main__location");
-  tourSectionLocation.appendChild(tourLocation);
-  tourLocation.innerText = "LOCATION";
+    let tourLocation = document.createElement("div");
+    tourLocation.classList.add("location__input");
+    tourLocation.classList.add("main__location");
+    tourSectionLocation.appendChild(tourLocation);
+    tourLocation.innerText = "LOCATION";
 
-  let tourLocationInfo = document.createElement("div");
-  tourLocationInfo.classList.add("venue__title");
-  tourSectionLocation.appendChild(tourLocationInfo);
-  tourLocationInfo.innerText = showContentObj.location;
+    let tourLocationInfo = document.createElement("div");
+    tourLocationInfo.classList.add("venue__title");
+    tourSectionLocation.appendChild(tourLocationInfo);
+    tourLocationInfo.innerText = showContentObj.location;
 
-  let tourButton = document.createElement("div");
-  tourButton.classList.add("tour__button");
-  tourWrapper.appendChild(tourButton);
-  tourButton.innerText = "BUY TICKETS";
+    let tourButton = document.createElement("div");
+    tourButton.classList.add("tour__button");
+    tourWrapper.appendChild(tourButton);
+    tourButton.innerText = "BUY TICKETS";
   });
 };
-// I know that the headings are in every block. My priority was making sure I implemented the javascript correctly. I realised that my element headings were in my block too late on and had no time to remove them.
